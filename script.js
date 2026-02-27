f218b7f22d19530e70e9515dbeea04611d0964bb
Marco Antonio Dalessio <amtoniomarco91@gmail.com>
2026-02-27 11:33:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let contador = 0
+
+    function verificar(numero, elemento){
+        const passar = document.getElementById("passar")
+        const retry = document.getElementById("retry")
+    
+        const botoes = document.querySelectorAll(".answer-button");
+        contador++
+
+        if(numero === '1') {
+            elemento.style.backgroundColor = "rgb(139, 245, 135)"
+            passar.style.backgroundColor = "white"
+            passar.disabled = false
+            passar.style.cursor = "pointer"
+            block(botoes)
+           
+        }
+        else {
+            elemento.style.backgroundColor = "rgb(245, 135, 135)"
+        }
+
+        if(contador >= 2){
+            block(botoes)
+            retry.disabled = false
+            retry.style.cursor = "pointer"
+            retry.style.backgroundColor = "white"
+        }
+    }
+
+  
+
+function block(botoes) {
+    botoes.forEach(btn => {
+        btn.disabled = true;
+        btn.style.cursor = "not-allowed";
+        btn.style.opacity = "0.6";
+    });
+} 
