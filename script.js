5321052cf0cb12d88a333dacc59365bfe0bdc41c
Marco Antonio Dalessio <amtoniomarco91@gmail.com>
2026-03-06 11:05:15 -0300

    feat: sistema de temporizador 100% Funcional

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-tempo = 0;
 
-        passarTempo = setInterval(() => {
-            tempo++
-            document.getElementById('relogio').innerText = tempo;
-
-            if (tempo < 10) {
-                document.getElementById('relogio').innerText = '0' + tempo;
-            }
-
-            if (tempo > 10) {
-                document.getElementById('relogio').innerText = "Você perdeu!";
-                
-            }
-        }, 1000);
 
 let contador = 0
+
+    let segundos = 0;
+    const intervalId = setInterval(() => {
+          const retry = document.getElementById("retry")
+        const botoes = document.querySelectorAll(".answer-button");
+        segundos++;
+        console.log(segundos);
+
+        timer = document.getElementById('time').innerText = segundos
+
+        if (segundos >= 10) {
+            pararCronometro();
+            timer = document.getElementById('time').innerText = "False"
+            block(botoes)
+            retry.disabled = false
+            retry.style.cursor = "pointer"
+            retry.style.backgroundColor = "white"
+        }
+
+    }, 1000);
+
+    function pararCronometro() {
+        clearInterval(intervalId); 
+        console.log('Intervalo parado')
+    }
 
     function verificar(numero, elemento){
         const passar = document.getElementById("passar")
         const retry = document.getElementById("retry")
-    
         const botoes = document.querySelectorAll(".answer-button");
         contador++
+       
 
         if(numero === '1') {
             elemento.style.backgroundColor = "rgb(139, 245, 135)"
@@ -29,21 +40,30 @@ let contador = 0
             passar.disabled = false
             passar.style.cursor = "pointer"
             block(botoes)
-           
+            pararCronometro()
+            if (pararCronometro){
+                timer = document.getElementById('time').innerText = "True"
+            }
+
         }
         else {
             elemento.style.backgroundColor = "rgb(245, 135, 135)"
         }
 
         if(contador >= 2){
+            pararCronometro()
+            if (pararCronometro){
+                timer = document.getElementById('time').innerText = "False"
+            }
             block(botoes)
             retry.disabled = false
             retry.style.cursor = "pointer"
             retry.style.backgroundColor = "white"
         }
+
+        
     }
 
-  
 
 function block(botoes) {
     botoes.forEach(btn => {
@@ -52,3 +72,4 @@ function block(botoes) {
         btn.style.opacity = "0.6";
     });
 } 
+
